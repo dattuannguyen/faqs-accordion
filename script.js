662b0ce649e1b4c264e9ffaa9f5995174163aa83
dattuannguyen <dat.nt0609@gmail.com>
2024-02-01 13:59:49 +0100

    clean the code

--- script.js
+++ script.js
@@ -2,23 +2,6 @@
 const faqs = document.querySelectorAll(".question-and-answer")
 let icon = document.querySelectorAll(".icon")
 
-// faqs.forEach(faqs =>{
-//     faqs.addEventListener("click",()=>{
-//         // faqs.classList.toggle("active")
-//         // console.log(icon)
-//         // icon.src='./assets/images/icon-minus.svg'
-//         // icon.classlist.toggle("minus")
-//         toggle(faqs);
-//         console.log(faqs);
-//         console.log(icon[2]);
-//     })
-// })
-//
-// const toggle = (faqs) =>{
-//     faqs.classList.toggle("active");
-//     // icon.classlist.toggle("minus");
-// }
-
 
 for (let i=0 ; i< faqs.length;i++){
     faqs[i].addEventListener("click",()=>{
